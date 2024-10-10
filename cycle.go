6c4380d38416c4e6deea4431f2b53e9a9cf8adb6
Ezri Zhu <me@ezrizhu.com>
2024-10-10 13:59:15 -0400

    comm prom fix

--- cycle.go
+++ cycle.go
@@ -21,7 +21,7 @@ func (p *Prefix) update(site *ConfigSite) {
 		routesGauge.WithLabelValues(
 			p.prefix,
 			p.lastAdvSite.Name,
-			strconv.Itoa(p.lastAdvSite.Id),
+			strconv.Itoa(p.lastAdvSite.Id+2000),
 		).Set(0)
 		p.bgpWithdraw()
 	}
